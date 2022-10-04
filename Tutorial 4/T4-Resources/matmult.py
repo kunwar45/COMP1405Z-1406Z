81cc9bfdf120c9f2d9309464b778d7bca2cbebb9
Hassan Ali <thehassanali2@gmail.com>
2022-10-04 12:02:45 -0400

    Done T4

--- Tutorial 4/T4-Resources/matmult.py
+++ Tutorial 4/T4-Resources/matmult.py
@@ -1,21 +1,13 @@
 from math import sqrt
 
-
 def mult_scalar(matrix, scale):
 	for row in range(len(matrix)):
 		for element in range(len(matrix[row])):
 			matrix[row][element] = matrix[row][element] * scale
 	return matrix
 
-''' [ 1 2 3 ] 	[ 7 8 ]
-	[ 4 5 6 ]   [ 9 10 ]
-	            [ 11 12 ]
-	Step 1:
-	Get row 1 of a, get column 1 of b
-	Do row 1[0] x column 1 [0]
-'''
 def mult_matrix(a, b):
-	if len(a) != len(b[0]): #if the number of rows in a does not equal the number of columns in b 
+	if (len(a) != len(b[0])) and (len(b) != len(a[0])): #if the number of rows in a does not equal the number of columns in b 
 		return None
 	dotProduct = 0
 	newMatrix = []
@@ -33,5 +25,3 @@ def euclidean_dist(a,b):
 		return None 
 	#list comprehension, sums up all the squared differences and square roots the sum
 	return sqrt(sum([(a[0][i]-b[0][i])**2 for i in range(len(a[0]))])) 
-
-print(mult_matrix([[1,2,3],[4,5,6]], [[7,8],[9,10],[11,12]]))
