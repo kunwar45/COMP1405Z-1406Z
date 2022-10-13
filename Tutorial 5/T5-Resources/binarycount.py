23721ad2599d62e09d4bb8cb9696abf392885f37
Hassan Ali <thehassanali2@gmail.com>
2022-10-13 14:57:07 -0400

    Binary Count analysis updated

--- Tutorial 5/T5-Resources/binarycount.py
+++ Tutorial 5/T5-Resources/binarycount.py
@@ -39,7 +39,7 @@ for i in range(int(repeat/20)):
 	searchlistX.append(random.randint(0,maxval))
 
 for i in range(repeat):		
-	searchlistY.append(random.randint(0,1*maxval))
+	searchlistY.append(random.randint(0,10000*maxval))
 
 searchlistX.sort()
 searchlistY.sort()
@@ -58,9 +58,25 @@ start = time.time()
 for i in searchlistY:
 	count(searchlistX,i)
 end = time.time()
-print("binary search ", (bintime:=end-start))
-print("Linear search is", round(lintime/bintime), "times slower than binary search")
+print("binary count time ", (bintime:=end-start))
+print("Linear count is", round(lintime/bintime), "times slower than binary count")
 
+
+''' Short Analysis
+1. My binary search count is generally much faster than the built in conut
+2. The larger X is, the slower linear count is compared to binary count
+3. The larger the range of values in X, the faster binary count is compared to linear count
+4. When there are more values in Y that aren't in X, binary count is even more quick compared to linear count
+5. This makes sense since:
+    Linear count will become slower and slower compared to binary count the more values there are to search through.
+    
+    So when X is larger, linear count has to search through more elements which makes it slower
+    
+    When the range of values in X is large, there are less duplicates which makes binary count a bit faster since there will be less
+    to count
+
+    When there are more values in Y that aren't in X, the linear count has to go through the whole list
+'''
 #The larger the range is, the slower linear search is compared to binary search
 #When there's a larger proportion of numbers that don't exist in X, linear search is even more slow compared to binary search
 #The larger the size of Y and X, the slower linear search is compared to binary search
