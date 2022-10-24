8558a2f5f989eea1feb63c227f58d9ed4c276350
Kunwar Nir <knir45@gmail.com>
2022-10-23 20:32:30 -0400

    mac is superior

--- crawler.py
+++ crawler.py
@@ -58,7 +58,7 @@ def crawl(seed):
     start = time.time()
     createFiles(newDict)
     end = time.time()
-    print("Mac is inferior",end-start)
+    print("Mac is superior",end-start)
 
     return count
 
@@ -254,6 +254,6 @@ def deleteFolder(folder):
             deleteFolder(file_path)
     os.rmdir(folder)
 
-# print(crawl("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"))
+print(crawl("http://people.scs.carleton.ca/~davidmckenney/fruits/N-0.html"))
 # print(webdev.read_url("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/"))
 
